6223ec8f60b2af862d0b39a87fded3e3f2326c73
CHOILOCAL <giants1217@naver.com>
2022-03-25 17:10:42 +0900

    status 200 error시 조건 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -64,11 +64,16 @@ export default {
 function _fetchMovie(payload) {
     const { title, type, year, page } = payload
     const OMDB_API_KEY = '7035c60c'
-    const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}$page=${page}`
+    // const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}$page=${page}`
+    const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}`
 
     return new Promise((resolve, reject) => {
         axios.get(url)
             .then(res => {
+                console.log(res)
+                if (res.data.Error) {
+                    reject(res.data.Error)
+                }
                 resolve(res)
             }).catch((err) => {
             reject(err.message)
